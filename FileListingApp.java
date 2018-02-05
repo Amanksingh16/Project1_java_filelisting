7ecd3dafdc84955cb2dc80ca0e89db01f41c23c9
Aman Singh <35393404+Amanksingh16@users.noreply.github.com>
2018-02-04 22:19:28 -0800

    Update FileListingApp.java

--- FileListingApp.java
+++ FileListingApp.java
@@ -9,7 +9,7 @@ public class FileListingApp {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the path of the file which contains two paths");
 		String path = sc.nextLine();
-		String a = "" , b = "";                             // These all string values are used to read both of those two paths written in a text file
+		String a = "" , b = "";                  // These all string values are used to read both of those two paths written in a text file
 		String path1 = "" , path2 = "";
 		File file = new File(path);
 		FileReader f = null;
@@ -33,8 +33,8 @@ public class FileListingApp {
 				b = b + (char)c;
 			}
 		} 
-		path2 = b.substring(1, b.length()-1);                        /* This Substring is used outside the if statement as the starting value of b is 
-		                                                              taking new line character of a , so it starts from 1 to read exact path*/
+		path2 = b.substring(1, b.length()-1);                /* This Substring is used outside the if statement as the starting value of b is 
+		                                                        taking new line character of a , so it starts from 1 to read exact path*/
 
 		 File newpath = new File(path1);            // The Path is fetched from the file
 		 
@@ -53,7 +53,7 @@ public class FileListingApp {
 	// Checks if it is a directory , then list all the inside files and folders with their name and absolute path
 	// else list all the files in that root folder with their name and path
 	
-	public static void check(File files1[] , String path1 , String path2) throws IOException        	                                                              //  so to avoid printing it again this newpath1 is used in line 73
+	public static void check(File files1[] , String path1 , String path2) throws IOException        	                                                             
 	{
 		ArrayList <File> files = new ArrayList<File>();                  // To store the files details in a list
 		Iterator<File> itr = null;                                       // To read from that list
@@ -75,11 +75,11 @@ public class FileListingApp {
 			itr = files.iterator();
 			FileOutputStream fos = new FileOutputStream(path2,true);                   // Used to write in the files that appends the value
       	    bw = new BufferedWriter(new OutputStreamWriter(fos));                      /* This Buffered Writer is used for the function newline and 
-			                                                                              can also be used as a buffer storage*/  
+			                                                                  can also be used as a buffer storage*/  
             while(itr.hasNext())
             {
          	   f = (File) itr.next();        	                                        
-         	   bw.write("Name of the File :- "+f.getName());                       /* Print both file names and file paths to the CSV file */
+         	   bw.write("Name of the File :- "+f.getName());               /* Print both file names and file paths to the CSV file */
          	   bw.write(" | ");
          	   bw.write("Path :- "+f.getAbsolutePath()); 
          	   bw.newLine();
